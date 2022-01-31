e5f48c878fa03e69d792e0f4d26016566011fbf0
aprendendofelipe <aprendendofelipe@gmail.com>
2022-01-31 01:31:01 -0300

    show selected server

--- src/pages/servers.js
+++ src/pages/servers.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
@@ -42,7 +43,9 @@ export default function ServersPage(props) {
       <SideBar>
         <SideMenu
           username={username}
+          screen={screen}
           setScreen={setScreen}
+          iframeSrc={iframeSrc}
           setIframeSrc={setIframeSrc}
           servers={servers}
         />
@@ -55,10 +58,10 @@ export default function ServersPage(props) {
         : <iframe
           height={'100%'}
           width={'100%'}
-        frameBorder={0}
-        allowFullScreen={false}
-          // src={`https://aluracord-lfs9902.vercel.app/chat?username=${username}`} />
-          src={iframeSrc} />}
+          frameBorder={0}
+          allowFullScreen={false}
+          src={iframeSrc}
+      />}
     </HomeScreen>
   </>)
 }
@@ -68,36 +71,52 @@ function SideMenu(props) {
 
   return (
     <>
-      <button onClick={()=>{props.setScreen('main')}} >
-        <img
-          src={`https://github.com/${props.username}.png`}
-          alt={props.username}
-        />
-        </button>
-      <button onClick={()=>{props.setScreen('main')}} >
-        <svg
-          aria-hidden="false"
-          width={36}
-          height={36}
-          viewBox="0 0 24 24">
-          <path
-            fill={theme.colors.primary[500]}
-            d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z">
-        </path>
-        </svg>
+      <button onClick={() => {
+        props.setScreen('main')
+        props.setIframeSrc('')
+      }} >
+        <div>
+          <img
+            src={`https://github.com/${props.username}.png`}
+            alt={props.username}
+          />
+        </div>
+      </button>
+      <button onClick={() => {
+        props.setScreen('main')
+        props.setIframeSrc('')
+      }}>
+        <div>
+          <svg
+            aria-hidden="false"
+            width={36}
+            height={36}
+            viewBox="0 0 24 24">
+            <path
+              fill={theme.colors.primary[500]}
+              d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z">
+            </path>
+          </svg>
+        </div>
       </button>
       <ul>
       <li key={"a"}>
-              <button onClick={() => {
-                props.setScreen('main')
-              }} >
-                <img
-                  src="https://deltime.com.br/wp-content/uploads/2019/03/seguran%C3%A7a-de-datacenters-Deltime.jpg"
-                  alt="main chat"
-                />
-                {/* {server.name} */}
-              </button>
-            </li>
+        <button onClick={() => {
+            props.setScreen('main')
+            props.setIframeSrc('')
+          }}
+        >
+          <div>
+            <img
+              src="https://deltime.com.br/wp-content/uploads/2019/03/seguran%C3%A7a-de-datacenters-Deltime.jpg"
+              alt="main chat"
+            />
+          </div>
+          {props.screen == 'main' && <div className='mask'>
+                    {/* {server.name} */}
+                  </div>}
+        </button>
+      </li>
         {servers.map((server) => {
           return (
             <li key={server.url}>
@@ -105,11 +124,14 @@ function SideMenu(props) {
                 props.setIframeSrc(server.autoUser ? server.url + props.username : server.url)
                 props.setScreen('chat')
               }} >
-                <img
-                  src={server.imgSrc}
-                  alt={server.name}
-                />
-                {/* {server.name} */}
+                <div>
+                  <img
+                    src={server.imgSrc}
+                    alt={server.name}
+                  />
+                </div>
+                {((server.url == props.iframeSrc) || ((server.url + props.username) == props.iframeSrc))
+                  && <div className='mask' />}
               </button>
             </li>
           )
@@ -149,25 +171,25 @@ const HomeScreen = styled.div`
   @media(min-width: 640px) {
     padding: 16px;
     grid-gap: 16px;
-    grid-template-columns: 72px 1fr;
+    grid-template-columns: 78px 1fr;
   }
   @media(max-width: 639px) {
     padding: 6px;
     grid-gap: 6px;
-    grid-template-rows: 72px 1fr;
+    grid-template-rows: 78px 1fr;
   }
 `
 
 const SideBar = styled.div`
   background-color: ${theme.colors.neutrals[700]};
   display: flex;
-  height: 72px;
+  height: 78px;
   width: 100%;
-  /* align-items: center; */
-  padding-top: 12px;
-  padding-left: 12px;
+  padding-top: 7px;
+  padding-left: 7px;
   border-radius: 5px;
-  gap: 12px;
+  gap: 4px;
+  transition: .3s;
   @media(min-width: 640px) {
     flex-direction: column;
     height: calc(100vh - 32px);
@@ -180,19 +202,22 @@ const SideBar = styled.div`
     display: flex;
     margin: 0;
     padding: 0;
-    gap: 12px;
-    overflow: auto;
+    overflow: hidden;
     @media(min-width: 640px) {
+      gap: 4px;
+      overflow-y: auto;
       flex-direction: column;
       max-height: 100%;
-      height: calc(100vh - 158px);
+      height: calc(100vh - 164px);
       padding-top: 8px;
       padding-bottom: 8px;
     }
     @media(max-width: 639px) {
+      gap: 8px;
+      overflow-x: auto;
       flex-direction: row;
       max-height: 100%;
-      width: calc(100vw - 138px);
+      width: calc(100vw - 144px);
       padding-left: 8px;
       padding-right: 8px;
     }
@@ -200,25 +225,54 @@ const SideBar = styled.div`
 
   button {
     cursor: pointer;
-    background-color: ${theme.colors.neutrals[200]};
-    display: flex;
+    background-color: transparent;
     border: 0;
-    border-radius: 50%;
     box-shadow: none;
-    height: 48px;
-    width: 48px;
+    height: 56px;
+    width: 56px;
     padding: 0;
     align-items: center;
     justify-content: center;
-    overflow: hidden;
-    opacity: 1;
-    :hover {
-    border-radius: 40%;
-    opacity: .6;
+    @media(max-width: 639px) {
+      display: contents;
     }
 
-    img {
-      height: 100%;
+    :hover div {
+      border-radius: 40%;
+    }
+
+    div {
+      display: flex;
+      background-color: ${theme.colors.neutrals[200]};
+      height: 48px;
+      width: 48px;
+      margin-left: 6px;
+      border-radius: 50%;
+      overflow: hidden;
+      align-items: center;
+      justify-content: center;
+      @media(max-width: 639px) {
+        margin-left: 0px;
+        margin-top: 6px;
+      }
+      
+      img {
+        height: 100%;
+        z-index: 1;
+      }
+    }
+    .mask {
+      background-color: ${theme.colors.primary[200]};
+      position: relative;
+      top: -52px;
+      left: -4px;
+      height: 56px;
+      width: 56px;
+      z-index: 0;
+      @media(max-width: 639px) {
+        top: -58px;
+        left: -4px;
+      }
     }
   }
 `
