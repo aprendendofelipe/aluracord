41586f64dd1a245960efdaf059d84b7da3787461
aprendendofelipe <aprendendofelipe@gmail.com>
2022-02-03 17:57:28 -0300

    chat height

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -12,10 +12,10 @@ function ChatBox(props) {
             box-shadow: 0 2px 10px 0 rgb(0 0 0 / 20%);
             border-radius: 5px;
             background-color: ${theme.colors.neutrals[700]};
-            height: calc(100vh - 98px);
+            height: 100%;
             width: 800px;
             max-width: calc(100vw - 12px);
-            max-height: calc(100vh - 90px);
+            max-height: calc(100vh - 98px);
             padding: 12px;
           }
           @media screen and (min-width: 640px) {
